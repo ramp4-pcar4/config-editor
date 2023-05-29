b28ff92ae7643842743624f8a835eb2a7bf8c29c
mohsin-r <mohsin.reza@mail.utoronto.ca>
2023-05-28 21:00:27 -0400

    Set up navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import Loading from '@/components/loading.vue';
+import Navbar from './components/navbar.vue';
+
 import '@/styles.css';
 import { useI18n } from 'vue-i18n';
 import type { API } from './main';
@@ -13,15 +15,18 @@ const initializeStore = useInitializeStore();
 </script>
 
 <template>
-  <div class="editor-container w-full h-full">
+  <div class="editor-container h-full">
     <div
       v-if="!initializeStore.initialized"
       class="w-full h-full bg-[#1c1717] box-border flex items-center justify-center"
     >
       <loading />
     </div>
-    <div v-else class="m-5">
-      <h1 class="text-3xl font-bold">{{ t('title') }}</h1>
+    <div v-else class="p-5 w-full h-full absolute top-0 left-0 flex flex-col">
+      <h1 class="flex-0 text-3xl font-bold text-center">{{ t('title') }}</h1>
+      <div class="flex-1 mt-5 flex">
+        <navbar class="basis-1/5 h-full" />
+      </div>
     </div>
   </div>
 </template>
---
+++ src/components/navbar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center border-black border-2 text-lg">
+    <div class="w-full hover:bg-gray-200 cursor-pointer p-3 border-b-2 border-gray font-semibold">
+      Starting Fixtures
+    </div>
+    <div
+      class="flex items-center w-full hover:bg-gray-200 cursor-pointer p-3 border-b-2 border-gray font-semibold"
+    >
+      <div class="expand-toggle mr-3 pointer-events-none">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+          <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" />
+        </svg>
+      </div>
+      Configs
+    </div>
+    <div class="w-full hover:bg-gray-200 cursor-pointer p-3 border-b-2 border-gray font-semibold">
+      Options
+    </div>
+    <button class="mt-3 bg-black text-white p-2 hover:bg-gray-800 rounded-md w-4/5 font-semibold">
+      Preview
+    </button>
+  </div>
+</template>
