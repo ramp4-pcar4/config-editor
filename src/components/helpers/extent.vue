8d6ab4ac21d857eed44d82c0dfbd95ae5960f85a
mohsin-r <mohsin.reza@mail.utoronto.ca>
2024-02-29 15:39:55 -0500

    Interactive map extent editor

--- src/components/helpers/extent.vue
+++ src/components/helpers/extent.vue
@@ -2,7 +2,10 @@
 import Collapsible from '@/components/helpers/collapsible.vue';
 import InputHeader from '@/components/helpers/input-header.vue';
 import type { RampExtentConfig } from '@/definitions';
-import type { PropType } from 'vue';
+import { ref, type PropType, onMounted, inject, watch } from 'vue';
+
+// @ts-ignore
+import { createInstance, geo } from '@/lib/ramp.esm';
 
 const props = defineProps({
   title: {
@@ -26,6 +29,158 @@ const props = defineProps({
   }
 });
 
+const rampConfig = {
+  startingFixtures: ['appbar', 'basemap', 'mapnav', 'help'],
+  configs: {
+    en: {
+      map: {
+        extentSets: [
+          {
+            id: 'EXT_ESRI_World_AuxMerc_3857',
+            default: {
+              xmax: -5007771.626060756,
+              xmin: -16632697.354854,
+              ymax: 10015875.184845109,
+              ymin: 5022907.964742964,
+              spatialReference: {
+                wkid: 102100,
+                latestWkid: 3857
+              }
+            }
+          },
+          {
+            id: 'EXT_NRCAN_Lambert_3978',
+            default: {
+              xmax: 3549492,
+              xmin: -2681457,
+              ymax: 3482193,
+              ymin: -883440,
+              spatialReference: {
+                wkid: 3978
+              }
+            }
+          }
+        ],
+        caption: {
+          mapCoords: {
+            formatter: 'WEB_MERCATOR'
+          },
+          scaleBar: {
+            imperialScale: true
+          }
+        },
+        lodSets: [
+          {
+            id: 'LOD_NRCAN_Lambert_3978',
+            lods: geo.defaultLODs(geo.defaultTileSchemas()[0])
+          },
+          {
+            id: 'LOD_ESRI_World_AuxMerc_3857',
+            lods: geo.defaultLODs(geo.defaultTileSchemas()[1])
+          }
+        ],
+        tileSchemas: [
+          {
+            id: 'EXT_NRCAN_Lambert_3978#LOD_NRCAN_Lambert_3978',
+            name: 'Lambert Maps',
+            extentSetId: 'EXT_NRCAN_Lambert_3978',
+            lodSetId: 'LOD_NRCAN_Lambert_3978',
+            thumbnailTileUrls: ['/tile/8/285/268', '/tile/8/285/269'],
+            hasNorthPole: true
+          },
+          {
+            id: 'EXT_ESRI_World_AuxMerc_3857#LOD_ESRI_World_AuxMerc_3857',
+            name: 'Web Mercator Maps',
+            extentSetId: 'EXT_ESRI_World_AuxMerc_3857',
+            lodSetId: 'LOD_ESRI_World_AuxMerc_3857',
+            thumbnailTileUrls: ['/tile/8/91/74', '/tile/8/91/75']
+          }
+        ],
+        basemaps: [
+          {
+            id: 'baseNrCan',
+            name: 'Canada Base Map - Transportation (CBMT)',
+            description:
+              'The Canada Base Map - Transportation (CBMT) web mapping services of the Earth Sciences Sector at Natural Resources Canada, are intended primarily for online mapping application users and developers.',
+            altText: 'The Canada Base Map - Transportation (CBMT)',
+            layers: [
+              {
+                id: 'CBMT',
+                layerType: 'esri-tile',
+                url: 'https://maps-cartes.services.geo.ca/server2_serveur2/rest/services/BaseMaps/CBMT3978/MapServer'
+              }
+            ],
+            tileSchemaId: 'EXT_NRCAN_Lambert_3978#LOD_NRCAN_Lambert_3978'
+          },
+          {
+            id: 'baseEsriWorld',
+            name: 'World Imagery',
+            description:
+              'World Imagery provides one meter or better satellite and aerial imagery in many parts of the world and lower resolution satellite imagery worldwide.',
+            altText: 'World Imagery',
+            layers: [
+              {
+                id: 'World_Imagery',
+                layerType: 'esri-tile',
+                url: 'https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer'
+              }
+            ],
+            tileSchemaId: 'EXT_ESRI_World_AuxMerc_3857#LOD_ESRI_World_AuxMerc_3857',
+            attribution: {
+              text: {
+                disabled: true
+              },
+              logo: {
+                disabled: true
+              }
+            }
+          }
+        ],
+        initialBasemapId: 'baseEsriWorld'
+      },
+      layers: [],
+      fixtures: {
+        appbar: {
+          items: ['basemap']
+        },
+        mapnav: { items: ['fullscreen', 'help', 'home', 'basemap'] }
+      },
+      system: { animate: true }
+    }
+  }
+};
+
+let rampApi: any;
+
+onMounted(() => {
+  watch(rampInstance, () => {
+    if (!!rampInstance.value) {
+      rampApi = createInstance(rampInstance.value, rampConfig, {
+        loadDefaultFixtures: true,
+        loadDefaultEvents: true
+      });
+    }
+  });
+});
+
+const saveExtent = () => {
+  const rampExtent = rampApi.geo.map.getExtent();
+  const newExtent = {
+    xmin: rampExtent.xmin,
+    xmax: rampExtent.xmax,
+    ymin: rampExtent.ymin,
+    ymax: rampExtent.ymax,
+    spatialReference: {} as any
+  };
+  Object.keys(rampExtent.sr).forEach((k) => {
+    if (rampExtent.sr[k] !== undefined) {
+      newExtent.spatialReference[k] = rampExtent.sr[k];
+    }
+  });
+  emit('update:modelValue', newExtent);
+};
+
+const rampInstance = ref<HTMLDivElement>();
 const emit = defineEmits(['update:modelValue']);
 
 const onInput = (e: Event) => {
@@ -60,7 +215,11 @@ const onInput = (e: Event) => {
 
 <template>
   <Collapsible :title="props.title" :required="required" :description="description">
-    <div>
+    <div ref="rampInstance" class="w-full max-w-2xl h-96"></div>
+    <button @click="saveExtent" class="mt-2 bg-black text-white p-1 hover:bg-gray-800">
+      Set Map Extent As Config
+    </button>
+    <div class="mt-4">
       <div class="input-table">
         <div>
           <input-header title="xmin" required />
