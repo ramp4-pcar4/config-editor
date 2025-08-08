e10e3331d4776ab1eed0b6b94c16604cca76bec0
james-rae <james-rae@users.noreply.github.com>
2025-08-08 18:43:43 +0000

    Deploying to gh-pages from @ ramp4-pcar4/config-editor@3bbbc26a927a51026e749908888cba5cdb7997b7 🚀

---
+++ rezaree/assets/PointSnappingHint-ClwQ1VNw-JjEPb3R8.js
@@ -0,0 +1,1 @@
+import{T as e}from"./vec32-BuqRmYBM-Ue24KLLT.js";import{N as i}from"./SketchViewModel-T2dNp9Tt-DU_BI2bl.js";class o extends i{constructor(t,r,s){super(r,s),this.point=t}equals(t){return t instanceof o&&e(this.point,t.point)}}export{o as t};
