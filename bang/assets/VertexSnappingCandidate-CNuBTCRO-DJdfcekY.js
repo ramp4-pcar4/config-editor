0c07834efaf84ad0a7e2b4ea618fa72003b33f8d
Yi Lei Feng <fengyilei1@gmail.com>
2025-08-14 18:06:50 +0000

    Delete PR demo from GH pages bang

--- bang/assets/VertexSnappingCandidate-CNuBTCRO-DJdfcekY.js
+++
@@ -1,1 +0,0 @@
-import{ai as r,m as s}from"./SketchViewModel-T2dNp9Tt-CeN_ZXbL.js";import{t as e}from"./PointSnappingHint-ClwQ1VNw-C4B5Q4JC.js";class a extends r{constructor(t){super({...t,constraint:new s(t.targetPoint)})}get hints(){return[new e(this.targetPoint,this.isDraped,this.domain)]}}export{a};
--- bang/assets/useT9n-ao51JRwP-D0gy6knx.js
+++
@@ -1,1 +0,0 @@
-import{i7 as o}from"./index-DLCK036j.js";import{u as r}from"./jsxFactory-C5LxVioS-YIiuZGAA.js";const s=o(r);export{s};
