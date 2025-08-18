07215cac698ac889a9be54979aec3df8eb4f4cdd
james-rae <james-rae@users.noreply.github.com>
2025-08-18 18:45:24 +0000

    Deploying to gh-pages from @ ramp4-pcar4/config-editor@e6138f287d7c473c2a8b96436878bd0f328cf345 🚀

---
+++ bang/assets/VertexSnappingCandidate-CNuBTCRO-D6_rD_kJ.js
@@ -0,0 +1,1 @@
+import{ai as r,m as s}from"./SketchViewModel-T2dNp9Tt-CbYXcT7B.js";import{t as e}from"./PointSnappingHint-ClwQ1VNw-_8oxxj_J.js";class a extends r{constructor(t){super({...t,constraint:new s(t.targetPoint)})}get hints(){return[new e(this.targetPoint,this.isDraped,this.domain)]}}export{a};
---
+++ bang/assets/containsOperator-DGjsfj6h-CzZIfVDp.js
@@ -0,0 +1,1 @@
+import{u as r}from"./ProjectionTransformation-BocI5WO--CWYUc_pe.js";import{fromGeometry as m,fromGeometryToGXGeometry as n}from"./jsonConverter-DldrjI06-Do6vtf2K.js";import"./SimpleGeometryCursor-DSF_YyIm-TAMgMsbK.js";import"./index-DiOGazkl.js";const f=new r;function p(t,o){const e=m(t);return f.execute(e.getGeometry(),n(o),e.getSpatialReference(),null)}export{p as execute};
