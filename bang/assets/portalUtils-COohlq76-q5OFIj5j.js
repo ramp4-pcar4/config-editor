621f9624ec45525593e864561838ab4487a1a867
Yi Lei Feng <fengyilei1@gmail.com>
2025-08-26 18:31:21 +0000

    Delete PR demo from GH pages bang

--- bang/assets/portalUtils-COohlq76-q5OFIj5j.js
+++
@@ -1,1 +0,0 @@
-import{bq as e}from"./index-CQ8-0QQp.js";function l(r,n){return r===null?n:new e({url:r.field("url")})}export{l as n};
--- bang/assets/requestImageUtils-B2YN5Eb9-0o8U4qI7.js
+++
@@ -1,1 +0,0 @@
-import{N as r}from"./index-CQ8-0QQp.js";async function o(a,e){const{data:n}=await r(a,{responseType:"image",...e});return n}export{o as r};
