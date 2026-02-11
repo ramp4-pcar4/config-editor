60a2a4d5b2da3df24bac7a7ff9c1e5b6dfe481f5
Spencer Wahl <spencer.s.wahl@gmail.com>
2026-02-11 09:04:34 -0500

    layout changes

---
+++ src/components/starting-screen.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="flex flex-col h-full start-container">
+        <h2 class="">{{ t('editor.title') }}</h2>
+        <div class="subtitle">{{ t('start.welcome') }}</div>
+    <div class="flex justify-center items-center">
+        <button class="" @click="newConfig()">
+            <span class="mr-2"
+                ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#434343"
+                >
+                    <path
+                        d="M440-240h80v-120h120v-80H520v-120h-80v120H320v80h120v120ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"
+                    /></svg></span
+            >{{ t('start.new') }}
+        </button>
+        <span class="separator"></span>
+        <button @click="existingConfig()">
+            <span class="mr-2"
+                ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#434343"
+                >
+                    <path
+                        d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"
+                    /></svg></span
+            >{{ t('start.existing') }}
+        </button>
+    </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { API } from '@/index';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+const newConfig = () => {
+    ((window as any).ramp4EditorAPI as API).initialize();
+};
+
+const existingConfig = () => {
+    ((window as any).ramp4EditorAPI as API).initialize();
+};
+</script>
+
+<style lang="scss" scoped>
+h2 {
+    font-size: 36px !important;
+    line-height: 40px !important;
+    padding-top: 40px !important;
+    padding-bottom: 80px !important;
+}
+
+.subtitle {
+    font-size: 24px !important;
+    line-height: 32px !important;
+    margin-bottom: 20px !important;
+}
+
+.start-container {
+    /* margin-right: 10%;
+    margin-left: 10%; */
+}
+
+button {
+    @apply items-center mx-3 border-black border-solid border px-5 flex w-full justify-center;
+    height: 100px;
+    outline: none;
+    &:hover, &:focus {
+        background-color: #e7e7e7;
+    }
+}
+
+.separator {
+    width: 24px;
+}
+</style>
--- src/config-editor.vue
+++ src/config-editor.vue
@@ -1,10 +1,39 @@
+<template>
+    <div class="ramp4-config-editor h-full">
+        <StartingScreen v-if="!store.initialized && !library" />
+        <div v-else class="h-full flex flex-col">
+            <div class="flex items-center">
+                <h2 class="h-9 text-3xl font-semibold">{{ t('editor.title') }}</h2>
+                <span class="ml-auto"></span>
+                <button
+                    v-if="!library"
+                    class="black-bg-button"
+                    @click="() => createNew()"
+                >
+                    {{ t('editor.new') }}
+                </button>
+            </div>
+            <div class="main-container flex-grow mt-3 flex">
+                <Navbar class="config-navbar h-full flex-shrink-0" :library="library" />
+                <div class="flex-grow h-full pl-5 overflow-y-auto">
+                    <div v-if="store.editingTemplate === ''">
+                        {{ t('editor.startEditing') }}
+                    </div>
+                    <component v-else :is="editors[store.editingTemplate]"></component>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
 <script setup lang="ts">
 // this is the overall editor
 
-import Loading from '@/components/loading.vue';
+import StartingScreen from './components/starting-screen.vue';
 import Navbar from './components/navbar.vue';
 import StartingFixturesEditor from '@/components/starting-fixtures.vue';
 import FixturesEditor from './components/fixtures/fixtures.vue';
+import JsonInput from './components/json-input.vue';
 import LayersEditor from '@/components/layers/layers.vue';
 import MapEditor from '@/components/map/map.vue';
 import PanelsEditor from '@/components/panels.vue';
@@ -21,13 +50,23 @@ import { useStore } from '@/store';
 
 const { t } = useI18n();
 const store = useStore();
+let library: boolean = false;
 
 const editors: { [key: string]: any } = {
     fixtures: FixturesEditor,
+    json: JsonInput,
     layers: LayersEditor,
     map: MapEditor,
+    options: OptionsEditor,
     panels: PanelsEditor,
+    preview: Preview,
+    'starting-fixtures': StartingFixturesEditor,
     system: SystemEditor
+};
+
+const createNew = () => {
+    store.initialized = false;
+    store.editingTemplate = '';
 };
 
 onMounted(() => {
@@ -43,32 +82,12 @@ onMounted(() => {
         delay: [200, 0],
         offset: [0, 5]
     });
+
+    if (import.meta.env.MODE.includes('lib')) {
+        library = true;
+    }
 });
 </script>
-
-<template>
-    <div class="ramp4-config-editor h-full">
-        <div v-if="!store.initialized" class="w-full h-full bg-[#1c1717] box-border flex items-center justify-center">
-            <Loading />
-        </div>
-        <div v-else class="h-full flex flex-col">
-            <h1 class="flex-none h-9 text-3xl font-semibold">{{ t('editor.title') }}</h1>
-            <div class="grow mt-3 flex">
-                <Navbar class="basis-1/5 h-full" />
-                <div class="basis-4/5 h-full px-5">
-                    <StartingFixturesEditor v-if="store.editingTemplate === 'starting-fixtures'" />
-                    <OptionsEditor v-else-if="store.editingTemplate === 'options'" />
-                    <Preview v-else-if="store.editingTemplate === 'preview'" />
-                    <component
-                        v-else-if="store.editingLang !== ''"
-                        :is="editors[store.editingTemplate]"
-                        :key="`${store.editingTemplate}-${store.editingLang}`"
-                    ></component>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
 
 <style lang="scss">
 $font-list: 'Montserrat', -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji,
@@ -92,55 +111,71 @@ $font-list: 'Montserrat', -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica
         font-family: $font-list;
         line-height: 1.5;
     }
-}
 
-.input-table {
-    /**
+    .input-table {
+        /**
    * User input values.
    */
-    --grid-layout-gap: 100px;
-    --grid-column-count: 10;
-    --grid-item--min-width: min(250px, 100%);
+        --grid-layout-gap: 100px;
+        --grid-column-count: 10;
+        --grid-item--min-width: min(250px, 100%);
 
-    /**
+        /**
    * Calculated values.
    */
-    --gap-count: calc(var(--grid-column-count) - 1);
-    --total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
-    --grid-item--max-width: calc((100% - var(--total-gap-width)) / var(--grid-column-count));
+        --gap-count: calc(var(--grid-column-count) - 1);
+        --total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
+        --grid-item--max-width: calc((100% - var(--total-gap-width)) / var(--grid-column-count));
 
-    display: grid;
-    grid-template-columns: repeat(
-        auto-fill,
-        minmax(max(var(--grid-item--min-width), var(--grid-item--max-width)), 1fr)
-    );
-    column-gap: var(--grid-layout-gap);
-    row-gap: 16px;
+        display: grid;
+        grid-template-columns: repeat(
+            auto-fill,
+            minmax(max(var(--grid-item--min-width), var(--grid-item--max-width)), 1fr)
+        );
+        column-gap: var(--grid-layout-gap);
+        row-gap: 16px;
 
-    select,
-    input {
-        @apply block border border-black text-sm;
+        select,
+        input {
+            @apply block border border-black text-sm;
+        }
+
+        input[type='text'],
+        input[type='number'],
+        select {
+            @apply w-full p-2;
+        }
     }
 
-    input[type='text'],
-    input[type='number'],
-    select {
-        @apply w-full p-2;
-    }
-}
-
-.required:after {
-    content: ' *';
-    color: red;
-}
-
-.ramp-styles {
-    .p-5 {
-        padding: 5px;
+    .required:after {
+        content: ' *';
+        color: red;
     }
 
-    div:has(> button[content='About RAMP']) {
-        @apply sm:hidden;
+    .ramp-styles {
+        .p-5 {
+            padding: 5px;
+        }
+    }
+
+    .config-navbar {
+        width: 324px;
+    }
+
+    .main-container {
+        height: calc(100% - 40px);
+    }
+
+    .black-bg-button {
+        @apply bg-black text-white p-2 rounded-md;
+        outline: none;
+        border-width: 1px;
+        border-color: #000;
+        &:hover,
+        &:focus {
+            background-color: rgba(209, 213, 219, 1);
+            color: black;
+        }
     }
 }
 </style>
